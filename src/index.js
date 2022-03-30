9d7b7c42de767a48740836e2f979fc33061465bc
[Jesus] <[jesusabad12_@hotmail.com]>
2022-03-30 12:53:51 -0600

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import {saludar} from './js/components';
+import './style.css';
+
+
+const nombre = 'Jesus y diana';
+
+saludar(nombre);
---
+++ src/js/components.js
@@ -0,0 +1,16 @@
+import '../css/componentes.css';
+
+
+export const saludar  =  (nombre ) => {
+
+    console.log('Creando Etiqueta h1')
+
+    const h1  = document.createElement('h1');
+    h1.innerText  = `Hola Nosotros somos ${nombre} `;
+
+    document.body.append(h1);
+
+    //img
+
+}
+
